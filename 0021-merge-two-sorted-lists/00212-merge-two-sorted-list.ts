3adf9c7eb710f621ba2a4f60b48d1a7465ee84cd
Msalmi <mmsalmi66@gmail.com>
2025-11-23 16:43:24 +0100

    merge two list using Recursion function

---
+++ 0021-merge-two-sorted-lists/00212-merge-two-sorted-list.ts
@@ -0,0 +1,17 @@
+function mergeTwoListsRecursion(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+
+    if(list1==null){
+        return list2;
+    }
+    if(list2 == null){
+        return list1;
+    }
+    if(list1.val <list2.val){
+           list1.next=mergeTwoListsRecursion(list1.next,list2);
+           return list1;
+    }
+    else {
+        list2.next=mergeTwoListsRecursion(list1,list2.next);
+        return list2;
+    }
+}
