48ddac126e3dcd7384f786b0acf40f9aee861699
Med Amine Msalmi <167647808+med-amine-msalmi@users.noreply.github.com>
2025-11-27 00:02:08 +0100

    Time: 0 ms (100%), Space: 45.5 MB (16.62%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -10,12 +10,15 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if(head==null || head.next==null)
-            return head;
-        head.next=deleteDuplicates(head.next);
-        if(head.next.val==head.val)
-            return head.next;
-    
+        ListNode current=head;
+        while(current!=null){
+            if(current.next!=null && current.next.val==current.val){
+                current.next=current.next.next;
+            }
+            else{
+                current=current.next;
+            }
+        }
         return head;
     }
 }
