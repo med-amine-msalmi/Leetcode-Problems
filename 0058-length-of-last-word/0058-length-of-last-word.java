22f0d69c7d73139376dd789b8418affcd70a64e8
Med Amine Msalmi <167647808+med-amine-msalmi@users.noreply.github.com>
2025-11-27 14:06:34 +0100

    Time: 1 ms (30.3%), Space: 43.4 MB (9.85%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,7 +1,14 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String str=s.trim();
-        String[] words=str.split(" ");
-        return (words[words.length-1]).length();
+        String[] words=s.split(" ");
+        String finalStr=words[words.length-1];
+        int i=2;
+        while(finalStr==" " && i<words.length+1){
+            finalStr=words[words.length-i];
+            i++;
+        }
+        return finalStr.length();
+
+       
     }
 }
