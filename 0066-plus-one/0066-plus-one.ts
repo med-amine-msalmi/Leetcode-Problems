cc50027088c988ba0ddf8d514794d320055e0b4b
Med Amine Msalmi <167647808+med-amine-msalmi@users.noreply.github.com>
2025-11-27 17:08:10 +0100

    Time: 0 ms (100%), Space: 53.6 MB (97.21%) - LeetHub

--- 0066-plus-one/0066-plus-one.ts
+++ 0066-plus-one/0066-plus-one.ts
@@ -1,15 +1,11 @@
 function plusOne(digits: number[]): number[] {
-    if(digits[digits.length-1]!=9){
-        digits[digits.length-1]++;
-        return digits;}
-    let i=digits.length-1;
-    while(digits[i]==9 && i>=0){
-        digits[i]=0;
-        i--;
+    for(let i=digits.length-1;i>=0;i--){
+        if(digits[i]==9)
+            digits[i]=0;
+        else{
+            digits[i]++;
+            return digits;}
     }
-    if(i<0)
-        digits.splice(i+1,0,1);
-    else
-        digits[i]++;
+    digits.splice(0,0,1);
     return digits;
 };
