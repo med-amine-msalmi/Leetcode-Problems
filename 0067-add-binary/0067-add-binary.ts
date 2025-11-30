1156908dc4d1c4d52ca7592862a80f7ec5d34eaa
Med Amine Msalmi <167647808+med-amine-msalmi@users.noreply.github.com>
2025-11-30 17:43:14 +0100

    Time: 0 ms (100%), Space: 56.5 MB (76.21%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.ts
@@ -0,0 +1,23 @@
+function addBinary(a: string, b: string): string {
+    let i=a.length-1;
+    let j=b.length-1;
+    let carryOut=0;
+    let output:string="";
+    while(i>=0 || j>=0){
+        let sum=0;
+        if(i>=0){
+            sum+=Number(a[i]);
+            i--;
+        }
+        if(j>=0){
+            sum+=Number(b[j]);
+            j--;
+        }
+        sum+=carryOut;
+        carryOut=Math.floor(sum/2);
+        output=(sum%2)+output;
+    }
+    if(carryOut)
+        output="1"+output;
+    return output;
+};
