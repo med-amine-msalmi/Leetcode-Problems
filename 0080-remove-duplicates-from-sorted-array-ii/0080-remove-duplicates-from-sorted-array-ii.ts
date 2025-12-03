c5ea3df3f17ef9fb142857d0a2d09efb6f24acd7
Med Amine Msalmi <167647808+med-amine-msalmi@users.noreply.github.com>
2025-12-04 00:02:10 +0100

    Time: 50 ms (84.95%), Space: 57.7 MB (98.21%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.ts
@@ -0,0 +1,11 @@
+function removeDuplicates(nums: number[]): number {
+    let i = 0; 
+
+    for (let j = 0; j < nums.length; j++) {
+        if (i < 2 || nums[j] !== nums[i - 2]) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+}
