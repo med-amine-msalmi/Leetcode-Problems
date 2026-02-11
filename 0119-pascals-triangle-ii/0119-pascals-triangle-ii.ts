240c0487abf688ad6fbf3976331b8a90dd335b75
Med Amine Msalmi <167647808+med-amine-msalmi@users.noreply.github.com>
2026-02-11 20:41:11 +0100

    Time: 0 ms (100%), Space: 55 MB (78.37%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.ts
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.ts
@@ -1,13 +1,11 @@
 function getRow(rowIndex: number): number[] {
 
-    let pascalTriangle:number[][]=[];
-    for(let c=0;c<=rowIndex;c++){
-        let arrayOfRow:number[]=Array(c+1).fill(1);
-        for(let k=1;k<c;k++){
-            arrayOfRow[k]=pascalTriangle[c-1][k-1]+pascalTriangle[c-1][k];
+    let row:number[]=Array(rowIndex+1).fill(0);
+    row[0]=1;
+    for(let c=1;c<=rowIndex;c++){
+        for(let k=c;k>0;k--){
+            row[k]+=row[k-1];
         }
-        if(c==rowIndex)
-            return arrayOfRow;
-        pascalTriangle.push(arrayOfRow);
-    } 
+    }
+    return row;
 };
