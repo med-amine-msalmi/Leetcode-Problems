5e7d0e623cfee87ac35799e5706b7915b5bb0c6c
Med Amine Msalmi <167647808+med-amine-msalmi@users.noreply.github.com>
2026-09-05 19:18:56 +0100

    Time: 48 ms (23.03%), Space: 80.7 MB (50.79%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.ts
@@ -0,0 +1,17 @@
+function containsNearbyDuplicate(nums: number[], k: number): boolean {
+    const window = new Set<number>();
+
+    for (let i = 0; i < nums.length; i++) {
+        if (window.has(nums[i])) {
+            return true;
+        }
+
+        window.add(nums[i]);
+
+        if (window.size > k) {
+            window.delete(nums[i - k]);
+        }
+    }
+
+    return false;
+}
