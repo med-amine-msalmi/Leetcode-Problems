5d8b9219cc285ef3a0d213092d9997433d299529
Med Amine Msalmi <167647808+med-amine-msalmi@users.noreply.github.com>
2026-09-13 13:25:30 +0100

    Time: 0 ms (100%), Space: 55.2 MB (42.31%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.ts
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.ts
@@ -1,17 +1,19 @@
 function convertToTitle(columnNumber: number): string {
     let title = "";
+    // map={ 1:A,  26:Z}
 
     while (columnNumber > 0) {
-        const remainder = columnNumber % 26;
-
-        if (remainder === 0) {
-            title = "Z" + title;
-            columnNumber = Math.floor(columnNumber / 26) - 1;
-        } else {
-            title = String.fromCharCode(remainder + 64) + title;
-            columnNumber = Math.floor(columnNumber / 26);
-        }
+       let remainder=columnNumber % 26; 
+       if(remainder ==0){
+            title="Z"+title
+            columnNumber--
+       }
+        else {
+            title=String.fromCharCode(64+remainder)+title }
+        columnNumber=Math.floor(columnNumber/26)
     }
-
     return title;
 }
+
+// remainder === 0 donc Z 
+// 
