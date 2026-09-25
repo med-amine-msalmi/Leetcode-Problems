f2ee745897c975fab9128e1239e80ddbab0a6480
Med Amine Msalmi <167647808+med-amine-msalmi@users.noreply.github.com>
2026-09-25 17:12:09 +0100

    Time: 20 ms (59.74%), Space: 65.2 MB (54.55%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.ts
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.ts
@@ -1,15 +1,18 @@
 function findLHS(nums: number[]): number {
-    let longestHS=0;
-    let startedWindow=0
-    nums.sort((a,b)=>a-b)
-    for(let i=0;i<nums.length;i++){
-        while(Math.abs(nums[i]-nums[startedWindow])>1){
-            startedWindow++
+    let longestHS=0
+    let map=new Map();
+    for(let n of nums){
+        if(!map.has(n)) map.set(n,1)
+        else { map.set(n,map.get(n)+1)}
+        let predecessor=n-1;
+        let successor=n+1
+        if(map.has(predecessor) ){
+            longestHS=Math.max(longestHS,map.get(n) + map.get(predecessor))
         }
-        if(Math.abs(nums[i]-nums[startedWindow])==1)
-            longestHS=Math.max(longestHS,Math.abs(i-startedWindow)+1)
-    }
-    return longestHS;
-};
+        if(map.has(successor)){
+            longestHS=Math.max(longestHS,map.get(n)+map.get(successor))
+        }
+    }  
+    return longestHS;  
+}
 
-//[1,2,2,3,3,3,4,4,5]
